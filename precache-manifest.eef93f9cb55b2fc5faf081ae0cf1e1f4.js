8c2d5d457334b4f9951c940e0e1e3ce21593befd
Sai Teja Muddana <saitejamuddana@gmail.com>
2019-12-05 09:21:39 -0600

    Updates

--- precache-manifest.eef93f9cb55b2fc5faf081ae0cf1e1f4.js
+++ precache-manifest.eef93f9cb55b2fc5faf081ae0cf1e1f4.js
@@ -1,27 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2233099ca733d47c4cb04c384a67bd17",
+    "revision": "4cf0b03c9fd84a2505975129768ec21a",
     "url": "/index.html"
   },
   {
-    "revision": "7ae41e85925f327bb5d5",
-    "url": "/static/css/main.1818e0dc.chunk.css"
+    "revision": "604d5aee805a211a8603",
+    "url": "/static/css/main.34de6062.chunk.css"
   },
   {
-    "revision": "56c2b428f094edd6e995",
-    "url": "/static/js/2.33cb38d7.chunk.js"
+    "revision": "841d7eb1bc4c3f5f8964",
+    "url": "/static/js/2.f6b422e6.chunk.js"
   },
   {
-    "revision": "7ae41e85925f327bb5d5",
-    "url": "/static/js/main.92f59ead.chunk.js"
+    "revision": "604d5aee805a211a8603",
+    "url": "/static/js/main.bc2279a3.chunk.js"
   },
   {
     "revision": "4d5b4449eca69dca7740",
     "url": "/static/js/runtime-main.77d3c816.js"
-  },
-  {
-    "revision": "e6ffd43f7e67f7f863f1fd98b6b3c5a5",
-    "url": "/static/media/aa.e6ffd43f.png"
   },
   {
     "revision": "168cc7f438eb6f7546718c2e1a30b809",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.99ebb669811e79b8f5fa2423ffe52329.js"
+  "/precache-manifest.eef93f9cb55b2fc5faf081ae0cf1e1f4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
