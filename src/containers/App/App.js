7a1c81d09a55465992c1dbf9f354dc15a847ae78
Sai Teja Muddana <saitejamuddana@gmail.com>
2019-12-05 09:20:01 -0600

    In component styling using M-UI

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,38 +1,90 @@
 import React from "react";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {makeStyles} from '@material-ui/core';
 
-import "./App.css";
+const useStyles = makeStyles(theme=>({
 
-function App() {
+  App: {
+    textAlign: 'center',
+  },
+  
+  AppLogo: {
+    height: '40vmin',
+  },
+  
+  AppHeader: {
+    backgroundColor: '#282c34',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: '2rem',
+    color: 'white',
+  },
+  AppBody1 :{
+    backgroundColor: '#282c34',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'left',
+    fontSize: '2rem',
+    color: 'white',
+  },
+  AppBody2: {
+    backgroundColor: '#ffffff',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'row-reverse',
+    alignItems: 'center',
+    justifyContent: 'right',
+    fontSize: '2rem',
+    color: 'black',
+  },
+  AppBody3: {
+    backgroundColor: '#927346',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    fontSize: '2rem',
+    color: 'white',
+  },
+  
+  AppLink: {
+    color: '#09d3ac',
+  }
+  
+
+}));
+
+const App = () => {
+  const classes = useStyles();
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className={classes.App}>
+      <Router >
+      <header className={classes.AppHeader}>
         <p>How To Demo.me</p>
       </header>
-      <div className='App-Body-1'>
-        <img src={require("./aa.png")} alt='apparition' />
-        <div>
-          <p>
-            its <b>MY</b> demo website
-          </p>
-          <p>YEAH, its incomplete just like this Ancient Apparition</p>
-        </div>
-      </div>
-      <div className='App-Body-2'>
-        <img src={require("./drow.png")} alt='drow' />
+      <div className={classes.AppBody2}>
+        <img src={require("../Assets/drow.png")} alt='drow' />
         <div>
           <p>Currently Demoing howdoiplay.com</p>
         </div>
       </div>
-      <div className='App-Body-3'>
+      <div className={classes.AppBody3}>
         <div>
           <p>This content originally belongs to this guy here -></p>
 
           <p>Click on Him to go see the original -></p>
         </div>
         <a href='https://howdoiplay.com' target='blank'>
-          <img src={require("./tsunami.png")} alt='tsunami643' />
+          <img src={require("../Assets/tsunami.png")} alt='tsunami643' />
         </a>
       </div>
+      </Router>
     </div>
   );
 }
